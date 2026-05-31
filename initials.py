16e36c5e3890f44c46ca5d7b7ccf4f1e640a5669
Abdul Manaf Ali <2425400645@live.gctu.edu.gh>
2026-05-31 07:39:25 +0000

    Finished initials.py assignment

--- initials.py
+++ initials.py
@@ -1,7 +1,31 @@
+# File: initials.py
+# Description: Print out my initials in stylized large letters.
+# Assignment Number: 1
+#
+# Name: Abdul Manaf Ali
+# STUDENT ID: 2425400645
+# Email: 2425400645@live.gctu.edu.gh
+# Grader: Abdul Manaf
+#
+# On my honor, Abdul Manaf Ali, this programming assignment is my own work
+# and I have not provided this code to any other student.
 
 def main():
-  pass #code goes here
-
+    # This function prints my initials in stylized large letters.
+    print()
+    print("...AMA")
+    print()
+    print(".......AAAA............MMMM....MMMM............AAAA.........")
+    print(".......AAAA............MMMM....MMMM............AAAA.........")
+    print("......AA..AA...........MM.MM..MM.MM...........AA..AA........")
+    print("......AA..AA...........MM.MM..MM.MM...........AA..AA........")
+    print(".....AAAAAAAA..........MM..MMMM..MM..........AAAAAAAA.......")
+    print(".....AAAAAAAA..........MM..MMMM..MM..........AAAAAAAA.......")
+    print("....AA......AA.........MM...MM...MM.........AA......AA......")
+    print("....AA......AA.........MM...MM...MM.........AA......AA......")
+    print("...AA........AA...**...MM........MM...**...AA........AA...**")
+    print("...AA........AA...**...MM........MM...**...AA........AA...**")
+    print()
 
 
 main()
